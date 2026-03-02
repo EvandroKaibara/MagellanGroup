8fca9cb011701c930011c5d2d5cc1f7c2e5e3edd
EvandroKaibara <yujievandro@gmail.com>
2026-03-02 17:33:23 -0300

    Companies

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,11 @@
 import { Banner } from "../../components/Banner"
+import { Companies } from "../../components/Companies"
 
 export const Home = () => {
     return(
+        <>
         <Banner />
+        <Companies />
+        </>
     )
 }
