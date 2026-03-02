407fc6e30fb04c8bb6427024640b00c7b94568b0
EvandroKaibara <yujievandro@gmail.com>
2026-03-02 10:52:36 -0300

    Project Build + React Router

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/home/home"
+import { About } from "./pages/about/about"
+import { Contact } from "./pages/contact/contact"
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
